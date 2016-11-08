414887deaffea638e41616facae48f547047c954
zhanchunliang <zhanchunliang@ele.me>
2016-11-08 17:30:54 +0800

    generator & this & arrow

--- Closure.js
+++ Closure.js
@@ -24,6 +24,15 @@ function create_couner(init){
 		}
 	}
 }
+//error
+function cteate_couner2(init){
+	var x = init || 0;
+	inc : function (){
+		x += 1;
+		return x;
+	}
+	return inc;
+}
 
 var c1 = create_couner();
 c1.inc();//1
@@ -32,3 +41,32 @@ c1.inc();//2
 var c2 = create_couner(10);
 c1.inc();//11
 c1.inc();//12
+
+//箭头函数
+x => x*x;
+
+(x,y) => x*x +y*y;
+
+var fn3 = (x,y,...rest) => {
+var i, sum = x+y;
+for (var i = rest.length - 1; i >= 0; i--) {
+	sum += rest[i];
+}
+return sum;
+}
+
+//this指向生效,call()、apply()第一个参数无法对this重新绑定
+var obj = {
+	birth: 1990,
+	getAge: function(){
+		var b = this.birth;
+		var fn = ()=>new Date().getFullYear() - this.birth; //this指向obj
+		return fn();
+	}
+}
+
+
+
+
+
+
