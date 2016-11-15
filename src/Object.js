a6b8daf665f2175d72ae1e1df716ac04a3ef9f1a
zhanchunliang <zhanchunliang@ele.me>
2016-11-15 21:01:32 +0800

    test js

--- src/Object.js
+++ src/Object.js
@@ -15,4 +15,43 @@ function createMan(name){
 
 
 var zcl = createMan('zcl');
-zcl.
+
+
+//对象继承  protype
+function inherits(Child, Parent) {
+    var F = function () {};
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.prototype.constructor = Child;
+}
+
+function Student(props) {
+    this.name = props.name || 'Unnamed';
+}
+
+Student.prototype.hello = function () {
+    alert('Hello, ' + this.name + '!');
+}
+
+function PrimaryStudent(props) {
+    Student.call(this, props);
+    this.grade = props.grade || 1;
+}
+
+// 实现原型继承链:
+inherits(PrimaryStudent, Student);
+
+// 绑定其他方法到PrimaryStudent原型:
+PrimaryStudent.prototype.getGrade = function () {
+    return this.grade;
+};
+
+
+var p = document.getElementById('js');
+p.innerText = 'test';
+p.style.fontWeight = 'bold';
+p.innerHTML = 'ABC <span style="color:red">RED</span> XYZ';
+
+var newElement = document.createElement('div');
+p.appendChild(newElement);
+
